0e72d36fbbcf8728a54e8b12e5d02e7109b5a361
Ahmed <an0088039@gmail.com>
2023-08-11 14:28:34 +0300

    Initial commit

---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+  static Color primiaryColor = const Color(0xff5D9CEC);
+  static Color accentColor = const Color(0xffDFECDB);
+  static Color primiaryDarkColor = const Color(0xff141A2E);
+  static Color accentDarkColor = const Color(0xffFACC1D);
+  static Color white = Colors.white;
+}
